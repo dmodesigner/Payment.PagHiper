5b4f92b176466d6a638555b0b980074e55aa6a4e
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2023-08-22 11:40:56 -0300

    Adicionado o enumerador de Stauts de Resposta HTTP

---
+++ Domain/Enums/StatusHttpEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Enums
+{
+    public enum StatusHttpEnum
+    {
+        Ok = 200,
+        Success = 201,
+        BadRequest = 400,
+        Unauthorized = 401,
+        Forbidden = 403,
+        Unauthorized2 = 405,
+        NotFound = 404,
+        UnsupportedMediaType = 415,
+        InternalServerError = 500
+    }
+}
