f0a089e57702b14a09aa1be62f67b870f7b9eb3d
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2023-11-17 16:30:30 -0300

    Adicionado a classe StringConverterCustomization.cs

---
+++ Src/PagHiper/Customizations/StringConverterCustomization.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace PagHiper.Customizations
+{
+    public class StringConverterCustomization : System.Text.Json.Serialization.JsonConverter<string>
+    {
+        public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+
+            if (reader.TokenType == JsonTokenType.Number)
+            {
+                var stringValue = reader.GetInt32();
+
+                return stringValue.ToString();
+            }
+            else if (reader.TokenType == JsonTokenType.String)
+                return reader.GetString();
+
+            throw new JsonException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options) => writer.WriteStringValue(value);
+    }
+}
