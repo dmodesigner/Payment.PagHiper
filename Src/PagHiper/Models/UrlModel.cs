94884ff7008edb394b077a2e3c17af715da24322
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2023-11-17 16:56:17 -0300

    Removido a pasta Models.

--- Src/PagHiper/Models/UrlModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PagHiper.Models
-{
-    public class UrlModel
-    {
-        public string? CreateBankSlip { get; set; }
-        public string? CancelBankSlip { get; set; }
-        public string? ConsultBankSlip { get; set; }
-    }
-}
