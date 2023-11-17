3b4d4a4343ad232771a5518772acb2ff3302ba0e
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2023-11-17 16:41:03 -0300

    Adicionado as classes do Requests.

---
+++ Src/PagHiper/Requests/ConsultRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PagHiper.Requests
+{
+    public class ConsultRequest
+    {
+        [JsonPropertyName("token")]
+        public string? Token { get; set; }
+
+        [JsonPropertyName("apiKey")]
+        public string? ApiKey { get; set; }
+
+        [JsonPropertyName("status")]
+        public string? Status { get; set; }
+
+        [JsonPropertyName("transaction_id")]
+        public string? TransactionId { get; set; }
+    }
+}
---
+++ Src/PagHiper/Requests/ItemRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PagHiper.Requests
+{
+    public class ItemRequest
+    {
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        [JsonPropertyName("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonPropertyName("item_id")]
+        public string? ItemId { get; set; }
+
+        [JsonPropertyName("price_cents")]
+        public int PriceCents { get; set; }
+    }
+}
