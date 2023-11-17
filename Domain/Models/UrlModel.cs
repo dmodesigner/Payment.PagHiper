7cc41e13484120da0a4aeb712b6baf27583df6c9
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2023-11-17 16:57:55 -0300

    Removido os projetos que não sera mais usados.

--- Domain/Models/UrlModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Models
-{
-    public class UrlModel
-    {
-        public string CreateBankSlip { get; set; }
-        public string CancelBankSlip { get; set; }
-        public string ConsultBankSlip { get; set; }
-    }
-}
