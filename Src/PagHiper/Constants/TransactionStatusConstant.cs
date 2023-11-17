21b466c6fe13231be73241c4150a41021ea4071a
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2023-11-17 16:27:00 -0300

    Refatoração do projeto. Adicionado as Constants.

---
+++ Src/PagHiper/Constants/TransactionStatusConstant.cs
@@ -0,0 +1,13 @@
+﻿namespace PagHiper.Constants
+{
+    public class TransactionStatusConstant
+    {
+        public const string Pending = "pending";
+        public const string Reserved = "reserved";
+        public const string Canceled = "canceled";
+        public const string Completed = "completed";
+        public const string Paid = "paid";
+        public const string Processing = "processing";
+        public const string Refunded = "refunded";
+    }
+}
